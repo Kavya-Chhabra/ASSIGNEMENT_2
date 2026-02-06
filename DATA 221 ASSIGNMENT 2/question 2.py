5ec33dbb9388faee507080de3f7ea055d929adeb
Kavya-Chhabra <kavya.chhabra@ucalgary.ca>
2026-02-06 16:38:48 -0700

    changed variable names and fixed some logic

--- DATA 221 ASSIGNMENT 2/question 2.py
+++ DATA 221 ASSIGNMENT 2/question 2.py
@@ -11,57 +11,58 @@
 import string
 
 #first we must open the file and read its contents
-text_file = open("sample-file.txt", "r", encoding="utf-8")
+input_text_file = open("sample-file.txt", "r", encoding="utf-8")
 
 #now read the file
-file_contents = text_file.read()
+full_text_contents = input_text_file.read()
 
 #was done for debugging purposes to make sure code doesn't crash
-print("Chars in file:", len(file_contents))
+print("Chars in file:", len(full_text_contents))
 
 #good coding practice to always close a file
-text_file.close()
+input_text_file.close()
 
 #next we must split the text into seperate tokens
-raw_tokens = file_contents.split()
+raw_word_tokens = full_text_contents.split()
 
 #make a list to store all of the "clean" words
-cleaned_words = []
+cleaned_word_list = []
 
 #individually clean and filter each token
-for token in raw_tokens:
-    token = token.lower()
+for raw_token in raw_word_tokens:
+    cleaned_token = raw_token.lower()
 
     #make sure to remove punctuation
-    token = token.strip(string.punctuation)
+    cleaned_token = cleaned_token.strip(string.punctuation)
 
     #count how many letters are in each token
-    count_of_letters = 0
-    for char in token:
-        if char.isalpha():
-            count_of_letters += 1
+    alphabetic_character_count= 0
+    for character in cleaned_token:
+        if character.isalpha():
+            alphabetic_character_count += 1
     #only words wit more than 2 letters will be kept
 
-    if count_of_letters >= 2:
-        cleaned_words.append(token)
+    if alphabetic_character_count >= 2:
+        cleaned_word_list.append(cleaned_token)
 
 # a bigram is two consecutive words and we will create a list to store all of them
-bigrams = []
+bigram_list = []
 
 #scan through the word list and create pairs
-for i in range(len(cleaned_words)- 1):
-    bigram = cleaned_words[i] + " " + cleaned_words[i+1]
-    bigrams.append(bigram)
+for index in range(len(cleaned_word_list)- 1):
+    first_word = cleaned_word_list[index] + " " + cleaned_word_list[index+1]
+    second_word = cleaned_word_list[index + 1]
+    bigram_list.append(first_word + "" + second_word)
 
 # now we must convert the dictionary into a list of (bigram,count) pairs
 bigram_counts = {}
 
 #count the number of times each bigram appears
-for bg in bigrams:
-    if bg in bigram_counts:
-        bigram_counts[bg] +=1
+for bigram in bigram_list:
+    if bigram in bigram_counts:
+        bigram_counts[bigram] +=1
     else:
-        bigram_counts[bg] = 1
+        bigram_counts[bigram] = 1
 
 #now we sort the bigrams in highest to lowest frequency
 sorted_bigram = list(bigram_counts.items())
@@ -76,13 +77,13 @@ if len(sorted_bigram) < 5:
     number_of_bigrams_to_print = len(sorted_bigram)
 
 #looping through the number of bigrams we decided to print
-for i in range (number_of_bigrams_to_print):
+for rank in range (number_of_bigrams_to_print):
 
     #getting the words from the list(bigrams)
-    bigram = sorted_bigram[i][0]
+    bigram = sorted_bigram[rank][0]
 
     #counting how many times it appears
-    count = sorted_bigram[i][1]
+    count = sorted_bigram[rank][1]
 
     #printing
     print(bigram, "->", count)
